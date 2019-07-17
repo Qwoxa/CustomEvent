abb4a4fb79c5bfa3b013a3af644e3c3be301ba03
Qwoxa <47032953+Qwoxa@users.noreply.github.com>
2019-07-17 08:25:54 -0400

    Prod. version

--- renderer.js
+++ renderer.js
@@ -1,21 +1,125 @@
-document.addEventListener('shout', appendQuotation);
+(function() {
+    // Variables for HTML elements (sectons) / Button 'stop-publishing'
+    const statisticsSection = document.getElementById('statistics');
+    const articlesSection = document.getElementById('published-articles');
+    const stopBtn = document.getElementById('stop-publishing');
+    
+    
+    // The statistics obj is used to keep track of statistics
+    const statistics = {};
+    Touratte.authorsList.map((author) => { 
+        statistics[author._id] = 0;
+    });
+    
+    
+    // Event listeners for 'shout' and 'click' events.
+    // The handler for 'shout' is the publishArticle function.
+    // The handler for 'click' (on 'stop-publishing' btn) is the
+    // stop publishing function.
+    document.addEventListener( 'shout', publishArticle );
+    stopBtn.addEventListener( 'click', stopPublishing );
+
+    /**
+     * The function creates the table with statistics and appends
+     * the table to statisticsSection
+     */
+    (function initializeStatistics() {
+        const table = document.createElement('table');
+        
+        // Iterate all authors, creating for each a row and appending
+        // this row to the table.
+        Touratte.authorsList.map((author) => {
+            const authorRow = document.createElement('tr');
+
+            const authorName = document.createElement('td');
+            authorName.innerHTML = author.name;
+            
+            // Add attrible data-author-id to access the td element
+            // while updating statistics
+            const articlesCount = document.createElement('td');
+            articlesCount.innerHTML = 0;
+            articlesCount.setAttribute( 'data-author-id', author._id );
+
+            authorRow.append( authorName, articlesCount );
+            table.append( authorRow );
+        });
+
+        statisticsSection.append( table );
+    })();
+
+    
+    /**
+     * The function creates HTML element for the 'article', updates statistics,
+     * appends the created element to the 'articlesSection' and highlights
+     * the new article for 1 second.
+     */
+    function publishArticle(e) {
+      const { author, text } = e.detail;
+      const article = createArticleHTML( author, text );
+
+      updateStatistics( author );
+      articlesSection.append( article );
+      highlightArticle( article );
+    }
 
 
-function appendQuotation(e) {
-  console.log( e.detail );
-  const quote = document.createElement('blockquote');
-  const text = e.detail.text;
-  const author = e.detail.author;
-  
-  const textParagraph = document.createElement('p');
-  textParagraph.innerHTML = text;
-  
-  const authorNameParagraph = document.createElement('p');
-  authorNameParagraph.innerHTML = author.name;
-  authorNameParagraph.classList.add('align-left');
-  
-  quote.append( textParagraph );
-  quote.append( authorNameParagraph );
-  
-  document.body.append( quote );
-}
+    /**
+     * The function creates a block with two paragraphs: text and author name.
+     * @param {Object} author The object that contains id/name of the author.
+     * @param {String} text The randomly generated text (article).
+     * @return {Object} HTML DIV element with article.
+     */
+    function createArticleHTML(author, text) {
+      const article = document.createElement('div');
+
+      const articleText = document.createElement('p');
+      articleText.innerHTML = text;
+        
+      const authorSignature = document.createElement('p');
+      authorSignature.innerHTML = author.name;
+      authorSignature.classList.add('signature');
+
+      article.append( articleText );
+      article.append( authorSignature );
+
+
+      return article;
+    }
+
+
+    /**
+     * The function updates the statistics object, changes innerHTML of the needed
+     * td element of the statistics table.
+     * @param {Object} author The object that contains id/name of the author.
+     */
+    function updateStatistics(author) {
+        const newCount = ++statistics[author._id];
+        
+        const selector = `[data-author-id = "${author._id}"]`;
+        const tableColumn = document.querySelector( selector );
+        
+        tableColumn.innerHTML = newCount;
+    }
+
+    /**
+     * The function sets the background to be yellow, and 1 second later
+     * changes the background to the default color
+     * @return {Object} HTML DIV element with article.
+     */
+    function highlightArticle(article) {
+        article.style.background = '#FFDC00'; // should change
+        setTimeout(() => {
+            article.style.background = '';
+        }, 1000);
+    }
+
+
+    /**
+     * The function clearsTimeout, so the function triggerShoutEvent from
+     * the touratte.js module is no longer called
+     */
+    function stopPublishing() {
+        clearTimeout( Touratte.eventTimer );
+        alert( 'The publishing has been stopped successfully!' );
+    }
+})();
--- touratte.js
+++ touratte.js
@@ -1,45 +1,90 @@
-const authorsList = [
-  {
-    _id: 1,
-    name: 'Todd',
-  },
-  {
-    _id: 3,
-    name: 'Rob',
-  },
-  {
-    _id: 3,
-    name: 'Sevil',
-  },
-];
-
-
-const shoutEvent = new CustomEvent('shout', { cancelable: true } );
-
-function generateText(len) {
-  let str = '';
-
-  for (let i = 0; i < len; i++) {
-    let charCode = randInt( 1040, 1103 );
-    str += String.fromCharCode( charCode );
-  }
+/**
+ * In the global space the timer key for clearTimeout and authorsList will
+ * will available as a properties of Touratte object.
+ */
+const Touratte = (function() {
+    // list of authours with unique IDs
+    const authorsList = [
+      {
+        _id: 1,
+        name: 'Todd',
+      },
+      {
+        _id: 2,
+        name: 'Rob',
+      },
+      {
+        _id: 3,
+        name: 'Sevil',
+      },
+    ];
   
-  return str;
-}
+    
+    // Call the recursive triggerShoutEvent function in 1-5s.
+    let eventTimer = setTimeout( triggerShoutEvent, randInt(1000, 5000) );
+    
+    
+    /**
+     * Triggers shout event once in 1-5s, redeclaring eventTimer property in
+     * touratteModule obj, which is the key for clearTimeout. Works recursively.
+     * @return {Undefined}
+     */
+    function triggerShoutEvent() {
+      let shoutEvent = createShoutEvent();
+      document.dispatchEvent( shoutEvent );
+        
+      touratteModule.eventTimer = setTimeout( triggerShoutEvent, randInt(1000, 5000) );
+    }
+    
+    
+    /**
+     * Creates event with name shout and detail with the random 
+     * author from authorList and randomly generated text.
+     * @return {Object} The event with name shout.
+     */
+    function createShoutEvent() {
+        return new CustomEvent('shout', {
+          detail: {
+              text: generateText( 60 ),
+              author: authorsList[randInt( 0, authorsList.length - 1 )]
+          }
+      });
+    }
+    
+    
+    /**
+     * Generates random text, using characters from range 1040 - 1103
+     * @params {Number} len The length of the text.
+     * @return {String} The randomly generated text.
+     */
+    function generateText(len) {
+      let str = '';
 
-function randInt(min, max) {
-  let rand = min + Math.random() * (max + 1 - min);
-  return Math.floor(rand);
-}
+      for (let i = 0; i < len; i++) {
+        let charCode = randInt( 1040, 1103 );
+        str += String.fromCharCode( charCode );
+      }
 
+      return str;
+    }
 
-let shoutDispatcher = setTimeout(function initShoutEvent() {
-  document.dispatchEvent( shoutEvent );
-  shoutDispatcher = setTimeout( initShoutEvent, randInt(1000, 5000) );
-}, randInt(1000, 5000));
-
-
-setTimeout(() => {
-  clearTimeout(shoutDispatcher);
-  console.log('done');
-}, 10000);
+    
+    /**
+     * Generates random integers within the specified range.
+     * @params {Number} min The lower bound of the random number.
+     * @params {Number} The The upper bound of the random number.
+     * @return {Number} The randomly generated number.
+     */
+    function randInt(min, max) {
+      let rand = min + Math.random() * (max + 1 - min);
+      return Math.floor(rand);
+    }
+    
+    // to export
+    const touratteModule = {
+        eventTimer: eventTimer,
+        authorsList: authorsList
+    };
+    
+    return touratteModule;
+})();
